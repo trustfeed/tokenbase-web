cdd61eaf043362d06c84fa71fdb7ba3c502c96d9
noelyoo <innovation.noel@gmail.com>
2018-11-05 18:36:01 +0900

    Change spinner style

--- src/components/spinner/index.tsx
+++ src/components/spinner/index.tsx
@@ -3,20 +3,7 @@ import './spinner.css';
 
 const Spinner: React.SFC<{}> = () => (
   <div className="py-5 text-center">
-    <div className="lds-default">
-      <div />
-      <div />
-      <div />
-      <div />
-      <div />
-      <div />
-      <div />
-      <div />
-      <div />
-      <div />
-      <div />
-      <div />
-    </div>
+    <div className="spinner" />
   </div>
 );
 
