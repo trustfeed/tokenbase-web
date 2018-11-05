70e6f9a5e0bad74be9ab900441b17e41845f222e
noelyoo <innovation.noel@gmail.com>
2018-11-05 12:15:06 +0900

    Fix build

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@ import { I18nextProvider } from 'react-i18next';
 import i18n from './utils/i18n';
 import { RouterNode } from './routes';
 import registerServiceWorker from './registerServiceWorker';
-import { NotificationContainer } from 'react-notifications';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import '!style-loader!css-loader!bootstrap/dist/css/bootstrap.css';
@@ -21,7 +20,6 @@ ReactDOM.render(
       <I18nextProvider i18n={i18n}>
         <div>
           <RouterNode />
-          <NotificationContainer />
         </div>
       </I18nextProvider>
     </PersistGate>
