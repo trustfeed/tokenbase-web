f7308e7f8a092159872fd82a22114a83cc47431d
noelyoo <innovation.noel@gmail.com>
2018-11-09 21:47:18 +0900

    Fix reducer

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import user from './user/reducer';
 import token from './token/reducer';
 import crowdsale from './crowdsale/reducer';
+import persist from './persist/reducer';
 
 export default combineReducers({
   user,
   token,
-  crowdsale
+  crowdsale,
+  persist
 });
