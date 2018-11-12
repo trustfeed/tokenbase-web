c5680ad881ee86b293d82e99f3575f1d2fe3bf6f
noelyoo <innovation.noel@gmail.com>
2018-11-12 18:10:46 +0900

    improve gzip script

--- gzip.sh
+++ gzip.sh
@@ -1,1 +1,3 @@
-find build/static -type f -exec gzip -9 {} \; -exec mv {}.gz {} \;
+find build/static/js -type f -exec gzip -9 {} \; -exec mv {}.gz {} \;
+find build/static/css -type f -exec gzip -9 {} \; -exec mv {}.gz {} \;
+find build/service-worker.js -type f -exec gzip -9 {} \; -exec mv {}.gz {} \;
